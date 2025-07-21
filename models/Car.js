736d525d12bc515deda5a2ec5d2f90982f969b05
Oliver Levine <oliversfphillips@icloud.com>
2025-07-21 14:39:40 -0700

    comments added to Car.js

--- models/Car.js
+++ models/Car.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const carSchema = new mongoose.Schema({
-    make: {type: String, required: true},
-    model: {type: String, required: true},
-    year: {type: Number, required: true},
-    color: String,
-    isAvailable: {type: Boolean, default: true},
+    make: { type: String, required: true },          // e.g., Honda
+    model: { type: String, required: true },         // e.g., Civic
+    year: { type: Number, required: true },          // e.g., 2020
+    color: String,                                   // e.g., Blue
+    isAvailable: { type: Boolean, default: true }    // Available or not
 });
 
-// create exportable model
+// Create and export the Car model
 const Car = mongoose.model('Car', carSchema);
 module.exports = Car;
